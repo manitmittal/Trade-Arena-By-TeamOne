4e72c3ccccf2663ae8a98e877462f9f3e5a67b82
Sahil Tiwari <sahiltiwari.9898@gmail.com>
2021-04-10 01:22:45 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, redirect, render_template, request, session,url_for,jsonify
+from tempfile import mkdtemp
+from cs50 import SQL
+from flask_session import Session
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
