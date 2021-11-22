683911be1fabc42dfb5eea1c296c22fdc32e0fd5
manitmittal <71818369+manitmittal@users.noreply.github.com>
2021-11-22 10:45:28 +0530

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -87,34 +87,4 @@ def lookup_stock(symbol):
         return None
 
 
-# def lookup(symbol):
-#     # data = yf.download(tickers=symbol+'.NS', period='10m', interval='5m')
-#     l=cryptocompare.get_coin_list(format = True)
-#     # valid = nse.is_valid_code(symbol)
-#     if (symbol.upper() in l):
-#         # stockdetails = nse.get_quote(symbol)
-#         price = cryptocompare.get_price(symbol.upper(), currency='USDT')[symbol.upper()]['USDT']
-#         # data2 = yf.Ticker(symbol+".NS")
-#         # name = data2.info['longName']
-#         return {
-#             "name": symbol.upper(),
-#             "price": price,
-#             "symbol": symbol.upper()
-#         }
-#     else:
-#         return None
 
-
-# def is_valid(symbol):
-
-#     # data = yf.download(tickers=symbol+'.NS', period='10m', interval='5m')
-#     # Print data
-#     l=cryptocompare.get_coin_list(format = True)
-   
-#     # x = (len(data))
-#     if(symbol.upper() in l):
-#         return {
-#             "symbol": symbol.upper()
-#         }
-#     else:
-#         return None
